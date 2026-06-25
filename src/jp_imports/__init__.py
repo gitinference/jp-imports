4163340c30e5ea818fc5351c96616e143b5c3728
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2026-06-25 16:45:21 -0400

    fix(__init__.py): add import of `importlib.metadata.version` to get package version feat(__init__.py): update `__version__` assignment to use `importlib.metadata.version` for better compatibility and maintainability

--- src/jp_imports/__init__.py
+++ src/jp_imports/__init__.py
@@ -1,6 +1,7 @@
 from .utils import TradeUtils
 from .jp_imports import JPTrade
 
+from importlib.metadata import version
+
 __version__ = version("jp_imports")
 __all__ = ["TradeUtils", "JPTrade"]
-
