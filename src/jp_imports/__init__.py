e1536198481ae582e7633d7890d87ae65eeed76f
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2026-07-27 10:54:24 -0400

    fix(src/jp_imports/__init__.py): remove unused TradeUtils from __all__ as it was replace with pr_imports

--- src/jp_imports/__init__.py
+++ src/jp_imports/__init__.py
@@ -1,7 +1,6 @@
-from .utils import TradeUtils
 from .jp_imports import JPTrade
 
 from importlib.metadata import version
 
 __version__ = version("jp_imports")
-__all__ = ["TradeUtils", "JPTrade"]
+__all__ = ["JPTrade"]
