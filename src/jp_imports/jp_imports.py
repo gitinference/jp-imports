1830ee5ddedef3d954849f5356201c35bcc5b5db
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2026-08-13 08:56:20 -0400

    refactor(jp_imports/jp_imports.py): add net_exports and net_imports columns to the DataFrame for better analysis

--- src/jp_imports/jp_imports.py
+++ src/jp_imports/jp_imports.py
@@ -118,9 +118,17 @@ class JPTrade(TradeUtils):
 
         df = df.drop([c for c in df.columns if c.endswith("_right")])
 
-        df = df.with_columns(
-            pl.col(["imports", "exports", "imports_qty", "exports_qty"]).fill_null(0)
-        ).sort(group_by_keys)
+        target_metrics = ["imports", "exports", "imports_qty", "exports_qty"]
+        df = (
+            df.with_columns(pl.col(target_metrics).fill_null(0))
+            .sort(group_by_keys)
+            .with_columns(
+                net_exports=pl.col("exports") - pl.col("imports"),
+                net_imports=pl.col("imports") - pl.col("exports"),
+                net_exports_qty=pl.col("exports_qty") - pl.col("imports_qty"),
+                net_imports_qty=pl.col("imports_qty") - pl.col("exports_qty"),
+            )
+        )
 
         return df
 
